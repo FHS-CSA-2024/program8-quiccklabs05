adcdda6513b2b29c8a03e6f8b4454aced4e72e49
quiccklabs05 <adibiswas053@gmail.com>
2024-08-30 09:26:28 -0700

    Update Program.java

--- Program.java
+++ Program.java
@@ -1,7 +1,17 @@
 //import stuff here
 
 //Your code here
-
+public class Program8{
+    public static void main(String[]args){
+        int num1 = 13;
+        int num2 = 20;
+        int sum = num1 + num2;
+        int diff = num1 - num2;
+        System.out.print.ln("Enter number 1:" + num1);
+        System.out.println("Enter number 2:" + num2);
+        System.print.ln("Original numbers are" + );
+    }
+}
 //Paste console output below:
 /*
 
