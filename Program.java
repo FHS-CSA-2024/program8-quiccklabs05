0301d99228902c99cd4fcd15fd93d717044c6580
quiccklabs05 <adibiswas053@gmail.com>
2024-08-31 17:28:08 -0700

    Update Program.java

--- Program.java
+++ Program.java
@@ -7,13 +7,44 @@ public class Program8{
         int num2 = 20;
         int sum = num1 + num2;
         int diff = num1 - num2;
-        System.out.print.ln("Enter number 1:" + num1);
-        System.out.println("Enter number 2:" + num2);
-        System.print.ln("Original numbers are" + );
+        int product = num1 * num2;
+        double avg = (num1 + num2)/2.0;
+        
+        int absolute1 = num1 - num2;
+        int answer1;
+        answer1 = Math.abs(absolute1);
+        
+        int absolute2 = num1 + num2;
+        int answer2;
+        answer2 = Math.abs(absolute2);
+        
+        int max = (answer1 + num1 + num2)/2;
+        int min = (answer2 - answer1)/2;
+        
+        System.out.println("Enter number 1:" + " " + num1);
+        System.out.println("Enter number 2:" + " " + num2);
+        System.out.println("Original numbers are" + " " + num1 + " " + "and" + " " + num2);
+        System.out.println("Sum =" + " " + sum);
+        System.out.println("Difference =" + " " + diff);
+        System.out.println("Product =" + " " + product);
+        System.out.println("Average =" + " " + avg);
+        System.out.println("Absolute value =" + " " + answer1);
+        System.out.println("Maximum =" + " " + max);
+        System.out.println("Minimum =" + " " + min);
+        
+        
     }
 }
 //Paste console output below:
 /*
-
-
+Enter number 1: 13
+Enter number 2: 20
+Original numbers are 13 and 20
+Sum = 33
+Difference = -7
+Product = 260
+Average = 16.5
+Absolute value = 7
+Maximum = 20
+Minimum = 13
 */
